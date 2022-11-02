99a73cb4553d75a25715972eeada47e2de3dc169
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-11-02 20:21:49 +0200

    adds styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import Main from "./components/Main";
 
 function App() {
-	return <Main />;
+	return (
+		<div className="container--dark">
+			<Main />
+		</div>
+	);
 }
 
 export default App;
